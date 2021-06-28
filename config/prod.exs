0778104b988ceff677dc891ee714a48359facea1
Roumen Roupski <rroupski@splunk.com>
2021-06-28 11:34:15 -0700

    Add HTTPS

--- config/prod.exs
+++ config/prod.exs
@@ -12,16 +12,13 @@ config :logger,
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-#     config :schema, SchemaWeb.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [
-#         :inet6,
-#         port: 443,
-#         cipher_suite: :strong,
-#         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-#       ]
+config :schema, SchemaWeb.Endpoint,
+  https: [
+    port: 443,
+    cipher_suite: :strong,
+    keyfile: "/etc/ssl/private/splunk-selfsigned.key",
+    certfile: "/etc/ssl/certs/splunk-selfsigned.crt"
+  ]
 #
 # The `cipher_suite` is set to `:strong` to support only the
 # latest and more secure SSL ciphers. This means old browsers
