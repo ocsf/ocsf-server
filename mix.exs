7de0bf035ccb4a038df7b2b103617275bd7c13ff
Roumen Roupski <rroupski@splunk.com>
2022-06-27 13:44:22 -0700

    Update the schema module

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Schema.MixProject do
         ]
       ],
       app: :schema_server,
-      version: "1.13.0-#{build}",
+      version: "1.14.0-#{build}",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
