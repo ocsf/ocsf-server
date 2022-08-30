81d35c7cb4f29edd72ace08dd22fba94290068e0
Roumen Roupski <rroupski@splunk.com>
2022-08-30 16:25:11 -0700

    #11 Add profile attribute list hits
    Signed-off-by: Roumen Roupski <rroupski@splunk.com>

--- lib/schema_web/views/layout_view.ex
+++ lib/schema_web/views/layout_view.ex
@@ -1,3 +1,15 @@
 defmodule SchemaWeb.LayoutView do
   use SchemaWeb, :view
+
+  def format_profile(nil) do
+    ""
+  end
+
+  def format_profile(profile) do
+      Enum.reduce(profile[:attributes], [], fn {name, _}, acc ->
+        [Atom.to_string(name) | acc]
+      end)
+      |> Enum.join("\n")
+  end
+
 end
