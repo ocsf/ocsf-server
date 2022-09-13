867edf74367d2ae8bdcbf8e9d08239b336009f6c
Roumen Roupski <rroupski@splunk.com>
2022-09-13 15:05:42 -0700

    Update the schema module and version
    Signed-off-by: Roumen Roupski <rroupski@splunk.com>

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Schema.MixProject do
         ]
       ],
       app: :schema_server,
-      version: "2.13.1-0.18.0-#{build}",
+      version: "2.14.0-0.20.0-#{build}",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers() ++ [:phoenix_swagger],
