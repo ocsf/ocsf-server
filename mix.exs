c9bc332b83337279fe2cda349e3592680fefcc2f
Roumen Roupski <rroupski@splunk.com>
2022-10-12 15:36:02 -0700

    Bump up the version number
    Signed-off-by: Roumen Roupski <rroupski@splunk.com>

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Schema.MixProject do
         ]
       ],
       app: :schema_server,
-      version: "2.19.0-0.24.2-#{build}",
+      version: "2.20.0-0.25.2-#{build}",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers() ++ [:phoenix_swagger],
