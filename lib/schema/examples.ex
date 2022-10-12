a6b74f3b9f0ae4ba9d6af5eacf616a3ec13cb937
Roumen Roupski <rroupski@splunk.com>
2022-10-12 15:33:51 -0700

    Create the examples repo structure
    Signed-off-by: Roumen Roupski <rroupski@splunk.com>

---
+++ lib/schema/examples.ex
@@ -0,0 +1,46 @@
+defmodule Schema.Examples do
+  @moduledoc """
+    Creates the examples repo structure.
+  """
+
+  @readme_file "README.md"
+
+  @doc """
+  Creates the README files
+  """
+  @spec create_readme(binary()) :: any()
+  def create_readme(path) do
+    if File.dir?(path) do
+      case File.ls(path) do
+        {:ok, files} ->
+          Enum.each(files, fn file ->
+            path = Path.join(path, file)
+
+            if File.dir?(path) and !String.starts_with?(file, ".") do
+              create_readme_file(file, path)
+              create_readme(path)
+            end
+          end)
+
+        error ->
+          exit(error)
+      end
+    end
+  end
+
+  defp create_readme_file(name, path) do
+    file = Path.join(path, @readme_file)
+
+    if !File.exists?(file) do
+      case File.write(file, "# #{name} Examples") do
+        :ok ->
+          IO.puts("created README file  : #{file}")
+
+        {:error, reason} ->
+          IO.puts("unable to create file: #{file}. Error: #{reason}")
+      end
+    else
+      IO.puts("file already exists  : #{file} ")
+    end
+  end
+end
