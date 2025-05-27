4745aafc4e9432dd6e14d1e9bf41b76f2af093dc
arpitjain099 <arpitjain099@gmail.com>
2025-05-27 10:03:02 +0900

    Improve code quality

--- lib/schema.ex
+++ lib/schema.ex
@@ -510,7 +510,7 @@ defmodule Schema do
   end
 
   @doc """
-  Returns a randomly generated sample event, based on the spcified profiles.
+  Returns a randomly generated sample event, based on the specified profiles.
   """
   @spec generate_event(Cache.class_t(), Repo.profiles_t() | nil) :: map()
   def generate_event(class, profiles) when is_map(class) do
@@ -530,7 +530,7 @@ defmodule Schema do
   end
 
   @doc """
-  Returns randomly generated sample object data, based on the spcified profiles.
+  Returns randomly generated sample object data, based on the specified profiles.
   """
   @spec generate_object(Cache.object_t(), Repo.profiles_t() | nil) :: map()
   def generate_object(type, profiles) when is_map(type) do
